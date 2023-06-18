ff6a196af6d035ddc10fde1e6f2d9f06398e8d4e
SPQRw <129948926+SPQRw@users.noreply.github.com>
2023-06-18 11:03:47 +0600

    home 4 changes

--- home 4/second hw/home4.2.js
+++ home 4/second hw/home4.2.js
@@ -1,55 +1,31 @@
-const fs = require('fs');
+const fs = require("fs");
 
-// Функция для чтения JSON файла
-function readJSONFile(filename) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, 'utf8', (error, data) => {
-      if (error) {
-        reject(error);
-      } else {
-        try {
-          const jsonData = JSON.parse(data);
-          resolve(jsonData);
-        } catch (parseError) {
-          reject(parseError);
-        }
-      }
-    });
-  });
-}
-
-// Функция для записи JSON файла
-function writeJSONFile(filename, jsonData) {
-  return new Promise((resolve, reject) => {
-    const jsonString = JSON.stringify(jsonData);
-    fs.writeFile(filename, jsonString, 'utf8', (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-async function updateAndWriteJSONFile() {
-  const filename = './data.json';
+// Считываем содержимое файла
+fs.readFile("data.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
   try {
-    // Чтение JSON файла
-    const jsonData = await readJSONFile(filename);
-    console.log('Текущее значение count:', jsonData.count);
+    // Разбираем JSON-строку в объект
+    const data = JSON.parse(jsonString);
 
-    // Увеличение значения count на один
-    jsonData.count++;
+    // Выводим значение count в консоль
+    console.log(data.count);
 
-    // Запись обновленного JSON файла
-    await writeJSONFile(filename, jsonData);
-    console.log('Значение count успешно обновлено и записано в файл.');
+    // Увеличиваем значение count на 1
+    data.count++;
 
-  } catch (error) {
-    console.log('Произошла ошибка:', error);
+    // Перезаписываем файл
+    fs.writeFile("data.json", JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Файл успешно перезаписан!");
+    });
+  } catch (err) {
+    console.error("Ошибка:", err);
   }
-}
-
-updateAndWriteJSONFile();
+});
