c261b8806a3bf68eb4882416e9edc5f0077cfc4c
SPQRw <long.1996@bk.ru>
2023-06-26 22:22:18 +0600

    test

--- home 6/main.js
+++ home 6/main.js
@@ -1,6 +1,6 @@
 import { Application } from "./bin/app.js";
-import fs from 'fs';
-import ini from 'ini';
+import fs from "fs";
+import ini from "ini";
 
 const cfg_file = fs.readFileSync("./config.ini", "utf-8");
 const config = ini.parse(cfg_file);
