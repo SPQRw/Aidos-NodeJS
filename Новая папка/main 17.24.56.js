d5a6d86c9086911d35cbcbf9f7142ddc616a2c9d
SPQRw <long.1996@bk.ru>
2023-07-05 17:32:41 +0600

    hw10

---
+++ Новая папка/main 17.24.56.js
@@ -0,0 +1,13 @@
+import { Application } from "./bin/app.js";
+
+const config = {
+  database: {
+    file: "database.sdb",
+  },
+  server: {
+    port: 3000,
+  },
+};
+
+const app = new Application(config);
+app.start();
